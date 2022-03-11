4b4dd6ae7603315047bae5005a3111cfb3d79024
Jhon Natera <49314891+Jhonnaar@users.noreply.github.com>
2022-03-10 23:01:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 
 app.use(express.json())
 
-const characters = require('./characters')
-app.use('/characters',characters)
+const missions = require('./missions')
+const missions_objectives = require('./missions_objectives')
+app.use('/missions',missions)
+app.use('/missions_objectives', missions_objectives)
 
 app.listen(8080);
