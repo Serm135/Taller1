5687b6089c06d89f8df1389d50d41909b1974b7e
Serm135 <pinilloss@uninorte.edu.co>
2022-03-10 22:59:24 -0500

    a

--- player_characters.js
+++ player_characters.js
@@ -22,6 +22,16 @@ router.get('/:id',async (req,res) => {
     } 
 });
 
+router.get('/:player',async (req,res) => {
+    let player = req.params.player
+    us=findplus(data.player_characters,"id",player)
+    if (us) {
+        res.status(200).json(us)
+    }else{
+        res.status(204).json({message:'No Content'})
+    } 
+});
+
 router.post('/create',async (req,res) => {
     if (req.body.name && req.body.stats && req.body.level && req.body.title && req.body.model && req.body.player) {
         data.player_characters.push({name:req.body.name,stats:req.body.stats,level:req.body.level,title:req.body.title,model:req.body.model,player:req.body.player,id:cont++})
@@ -45,11 +55,15 @@ router.delete('delete/:id',async (req,res) => {
 });
 
 router.patch('update/:key',async (req,res) => {
-    let key=req.params.key
-    let index = data.player_characters.indexOf(find(data.player_characters,"id",req.body.id))
-    data.player_characters[index][key]=req.body[key]
-    console.log(data)
-    res.status(200).json({message: 'Success'})    
+    if (key=='player') {
+        res.status(204).json({message:'No es posible modificar el dueño de un personaje'})
+    }else{
+        let key=req.params.key
+        let index = data.player_characters.indexOf(find(data.player_characters,"id",req.body.id))
+        data.player_characters[index][key]=req.body[key]
+        console.log(data)
+        res.status(200).json({message: 'Success'})  
+    }
 });
 
 function find(lista,key,id) {
@@ -59,5 +73,14 @@ function find(lista,key,id) {
         }
     }
     return false;
+}
+function findplus(lista,key,id) {
+    const elements = []
+    for (let element of lista) {
+        if (element[key]==id) {
+            elements.push(element)
+        }
+    }
+    return elements;
 }
 module.exports = router;
--- players.js
+++ players.js
@@ -5,8 +5,8 @@ const data = require('./data');
 let cont = 0
 
 router.get('/',async (req,res) => {
-    if (data.character_stats.length!=0) {
-        res.status(200).json(data.character_stats)
+    if (data.players.length!=0) {
+        res.status(200).json(data.players)
     }else{
         res.status(204).json({message:'No Content'})
     }
@@ -14,7 +14,7 @@ router.get('/',async (req,res) => {
 
 router.get('/:id',async (req,res) => {
     let id = req.params.id
-    us=find(data.character_stats,"id",id)
+    us=find(data.players,"id",id)
     if (us) {
         res.status(200).json(us)
     }else{
@@ -23,8 +23,8 @@ router.get('/:id',async (req,res) => {
 });
 
 router.post('/create',async (req,res) => {
-    if ( req.body.attribute_1 && req.body.attribute_2 && req.body.attribute_3) {
-        data.character_stats.push({life:attribute_1*20,power:(attribute_1*10)+(attribute_2*25),magic:attribute_3*100,attribute_1:req.body.attribute_1,attribute_2:req.body.attribute_2,attribute_3:req.body.attribute_3,id:cont++})
+    if (req.body.name && req.body.last_login && req.body.password && req.body.username) {
+        data.players.push({name:req.body.name,last_login:req.body.last_login,password:req.body.password,username:req.body.username,id:cont++})
         console.log(data)
         res.status(200).json({message: 'Success'}) 
     }else{
@@ -35,9 +35,9 @@ router.post('/create',async (req,res) => {
 
 router.delete('delete/:id',async (req,res) => {
     let id = req.params.id
-    if(find(data.character_stats,"id",id)){
-        let index = data.character_stats.indexOf(find(data.character_stats,"id",id))
-        data.character_stats.splice(index,1)
+    if(find(data.players,"id",id)){
+        let index = data.players.indexOf(find(data.players,"id",id))
+        data.players.splice(index,1)
         res.status(200).json({message: 'Success'})
     }else{
         res.status(204).json({message:'No Content'})
@@ -45,15 +45,11 @@ router.delete('delete/:id',async (req,res) => {
 });
 
 router.patch('update/:key',async (req,res) => {
-    if (key=='life' || key == 'power' || key == 'magic') {
-        res.status(204).json({message:'No es posible modificar estos campos'})
-    }else{
-        let key=req.params.key
-        let index = data.character_stats.indexOf(find(data.character_stats,"id",req.body.id))
-        data.character_stats[index][key]=req.body[key]
-        console.log(data)
-        res.status(200).json({message: 'Success'})  
-    } 
+    let key=req.params.key
+    let index = data.players.indexOf(find(data.players,"id",req.body.id))
+    data.players[index][key]=req.body[key]
+    console.log(data)
+    res.status(200).json({message: 'Success'})    
 });
 
 function find(lista,key,id) {
