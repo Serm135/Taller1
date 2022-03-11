034178ed9f788718597b6e99993f2078055b4ee2
Serm135 <pinilloss@uninorte.edu.co>
2022-03-10 23:41:52 -0500

    a

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const characters_stats = require('./characters_stats')
 const images_2d = require('./images_2d')
 const models_3d = require('./models_3d')
 const player_characters = require('./player_characters')
+const items = require('./items')
 app.use('/missions',missions)
 app.use('/missions_objectives', missions_objectives)
 app.use('/players', players)
@@ -18,7 +19,7 @@ app.use('/characters', characters)
 app.use('/characters_stats', characters_stats)
 app.use('/images_2d', images_2d)
 app.use('/models_3d', models_3d)
-app.use('/player_characters', player_characters)
+app.use('/items', items)
 
 
 app.listen(8080);
